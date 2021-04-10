1c89fb0c42473b868dc8036f4ec5921f035e28e1
KyleLynnRas <kylelynn.ras@gmail.com>
2021-04-10 18:31:33 -0400

    Initial commit

---
+++ db/connection.js
@@ -0,0 +1,38 @@
+/// grab environment variables
+require("dotenv").config()
+/// mongoose
+const mongoose = require("mongoose")
+// merced logger 
+const { log } = require("mercedlogger")
+// Bring in our database string from .env or default string
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/defaultdb"
+
+///////////////////////////////////
+// Mongoose Configuration Object 
+///////////////////////////////////
+const config = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}
+
+///////////////////////////////////
+// Database Connection
+///////////////////////////////////
+mongoose.connect(MONGODB_URL, config)
+
+///////////////////////////////////
+// Handling Connection Events
+///////////////////////////////////
+mongoose.connection
+  // Event for When Connection Opens
+  .on("open", () => log.green("STATUS", "Connected to Mongo"))
+  // Event for When Connection Closes
+  .on("close", () => log.red("STATUS", "Disconnected from Mongo"))
+  // Event for Connection Errors
+  .on("error", error => log.red("ERROR", error))
+
+///////////////////////////////////
+// Exporting Our Connection
+///////////////////////////////////
+module.exports = mongoose
---
+++ db/seed.js
@@ -0,0 +1,21 @@
+// import the database connection
+const mongoose = require("./connection")
+
+
+//////////////////////////////////
+// Models
+/////////////////////////////////
+
+
+/////////////////////////////////
+// Seed
+/////////////////////////////////
+const seed = async () => {
+    //--- CODE GOES HERE
+    console.log("seed file")
+    //--------------------
+}
+
+
+// run seed function
+seed()
