c6135e57527f3cd7c7268a50c68851c86692686d
KyleLynnRas <kylelynn.ras@gmail.com>
2021-04-12 13:10:21 -0400

    Post authorization working

--- routes/posts.js
+++ routes/posts.js
@@ -3,32 +3,58 @@
 ////////////////////////////////
 const router = require("express").Router()
 const postsController = require("../controllers/posts")
+const User = require("../models/User")
+
+///////////////////////////////
+// Custom Middleware Functions
+////////////////////////////////
+//Add current user to req.user
+const addUser = async (req, res, next) => {
+    if(req.session.userId) {
+        req.user = await User.findById(req.session.userId)
+        next()
+    } else {
+        next()
+    }
+}
+
+//Check for authorization
+const isAuthorized = (req, res, next) => {
+    //check if user logged in
+    if (req.user) {
+        next()
+    } else {
+        res.redirect("/user/login")
+    }
+}
+
 
 ///////////////////////////////
 // Router Specific Middleware
 ////////////////////////////////
+router.use(addUser)
 
 ///////////////////////////////
 // Router Routes
 ////////////////////////////////
 
 //Index:
-router.get("/", postsController.index)
+router.get("/", isAuthorized, postsController.index)
 
 //Delete:
-router.delete("/:id", postsController.destroy)
+router.delete("/:id", isAuthorized, postsController.destroy)
 
 //Update:
-router.put("/:id", postsController.update)
+router.put("/:id", isAuthorized, postsController.update)
 
 //Create:
-router.post("/", postsController.create)
+router.post("/", isAuthorized, postsController.create)
 
 //Edit:
-router.get("/:id/edit", postsController.edit)
+router.get("/:id/edit", isAuthorized, postsController.edit)
 
 //Show:
-router.get("/:id", postsController.show)
+router.get("/:id", isAuthorized, postsController.show)
 
 
 
