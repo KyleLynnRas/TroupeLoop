6fa9ccbc0aacefb649beb6ed8529cd5847196afd
KyleLynnRas <kylelynn.ras@gmail.com>
2021-04-13 13:56:00 -0400

    Comment authorization working

--- routes/comments.js
+++ routes/comments.js
@@ -3,29 +3,54 @@
 ////////////////////////////////
 const router = require("express").Router()
 const commentsController = require("../controllers/comments")
+const User = require("../models/User")
+
+///////////////////////////////
+// Custom Middleware Functions
+////////////////////////////////
+//Add current user to req.user
+const addUser = async (req, res, next) => {
+    if(req.session.userId) {
+        req.user = await User.findById(req.session.userId)
+        next()
+    } else {
+        next()
+    }
+}
+
+//Check for authorization
+const isAuthorized = (req, res, next) => {
+    //check if user logged in
+    if (req.user) {
+        next()
+    } else {
+        res.redirect("/user/login")
+    }
+}
 
 ///////////////////////////////
 // Router Specific Middleware
 ////////////////////////////////
+router.use(addUser)
 
 ///////////////////////////////
 // Router Routes
 ////////////////////////////////
 
 //Delete:
-router.delete("/:id", commentsController.destroy)
+router.delete("/:id", isAuthorized, commentsController.destroy)
 
 //Update: /comments/commentId
-router.put("/:id", commentsController.update)
+router.put("/:id", isAuthorized, commentsController.update)
 
 //Create: /comments/postId
-router.post("/:id", commentsController.create)
+router.post("/:id", isAuthorized, commentsController.create)
 
 //Edit: comments/commentId
-router.get("/:id/edit", commentsController.edit)
+router.get("/:id/edit", isAuthorized, commentsController.edit)
 
 //Show: /comments/commentId
-router.get("/:id", commentsController.show)
+router.get("/:id", isAuthorized, commentsController.show)
 
 ///////////////////////////////
 // Export Router
