495ef6611a42081e78f7914d8f94b018ff345c78
KyleLynnRas <kylelynn.ras@gmail.com>
2021-04-17 17:32:35 -0400

    User show favorite comments/posts working

--- controllers/users.js
+++ controllers/users.js
@@ -75,34 +75,35 @@ const logOut = (req, res) => {
 //Show: 
 const show = async (req, res) => {
     const userID = req.params.id
-    const currentUser = await User.findById(userID).populate("posts").populate("comments")
-    //get all comments info for user, reformat date and make new array
-    const currentComments = currentUser.comments
-    let commentsArr =[]
-    for (let comment of currentComments) {
+    const currentUser = await User.findById(userID).populate("posts").populate("comments").populate("favPosts").populate("favComments")
+    //User favComments into array to reformat date
+    const favCommsArr = currentUser.favComments
+    let favComms = []
+    for (let comment of favCommsArr) {
         let dbComm = await Comment.findById(comment.id).populate("author").populate("post")
         // date format
         let dbCommDate = dayjs(comment.createdAt).format("MM-DD-YY")
         let dbCommObj = {text: dbComm.text, author: dbComm.author.username, date: dbCommDate, id: dbComm.id, postId: dbComm.post.id, postTitle: dbComm.post.title}
         // console.log(dbCommObj.author, dbCommObj.postId)
-        commentsArr.push(dbCommObj)
+        favComms.push(dbCommObj)
+        console.log(favComms)
     }
-    //get all user posts, pass to array 
-    const currentPosts = currentUser.posts
-    let postsArr = []
-    for (let post of currentPosts) {
+    //User favposts into array to reformat date 
+    const favPostsArr = currentUser.favPosts
+    let favPosts = []
+    for (post of favPostsArr) {
+        //pop post with author info 
         let dbPost = await Post.findById(post.id).populate("author")
         // date format
         let dbPostDate = dayjs(post.createdAt).format("MM-DD-YY")
         let dbPostObj = {title: dbPost.title, text: dbPost.text, author: dbPost.author.username, date: dbPostDate, id: dbPost.id}
-        // console.log(dbPostObj.author)
-        postsArr.push(dbPostObj)
+        favPosts.push(dbPostObj)
     }
-
+    // console.log(favPosts)
     res.render("users/show", {
-        comments: commentsArr,
+        comments: favComms,
         user: currentUser,
-        posts: postsArr
+        posts: favPosts
     })
 }
 
